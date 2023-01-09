b32bacba703d0bf6a359ebcb386cad3b10381252
mystee <quebec706@gmail.com>
2023-01-09 17:20:59 +0900

    memos 테이블 컬럼 추가

--- database/migrations/2023_01_09_080929_create_memos_table.php
+++ database/migrations/2023_01_09_080929_create_memos_table.php
@@ -15,7 +15,12 @@ class CreateMemosTable extends Migration
     {
         Schema::create('memos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->longText('content');
+            $table->integer('user_id');
+            $table->integer('status')->defalut('1');
+            // timestamp라고 써버리면 레코드 삽입 시, 갱신 시의 값이 들어가지 않기 때문에 DB::raw로 직접 쓰고 있습니다.
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
